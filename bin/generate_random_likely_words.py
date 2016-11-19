ca56ef353464fb01a5c9f3878cdced91373265d4
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-11-19 11:16:51 -0800

    simplify [#134524957]

--- bin/generate_random_likely_words.py
+++ bin/generate_random_likely_words.py
@@ -7,33 +7,30 @@ phoneme_chain_prob = file.load('phoneme_probabilities')
 phonetic_words = file.load('phonetic_words')
 words = file.load('words')
 
-def get_next_phoneme(current_phoneme, random_number):
-    current_next_phoneme_chance = 2.0
-    current_next_phoneme = ''
-    for transition_phoneme, chance in phoneme_chain_prob[current_phoneme].iteritems():
-        if chance >= random_number and chance < current_next_phoneme_chance:
-            current_next_phoneme_chance = chance
-            current_next_phoneme = transition_phoneme
-    return current_next_phoneme
+def get_next_phoneme(phoneme, random_number):
+	current_next_phoneme_chance = 2.0
+	current_next_phoneme = ''
+	for transition_phoneme, chance in phoneme_chain_prob[phoneme].iteritems():
+		if chance >= random_number and chance < current_next_phoneme_chance:
+			current_next_phoneme_chance = chance
+			current_next_phoneme = transition_phoneme
+	return current_next_phoneme
 
-def not_in_dict(word):
-    if word in phonetic_words:
-        index = phonetic_words.index(word)
-        print '{} (word exists already: {})'.format(word, words[index])
-        return False
-    else:
-        return True
+def present(word_arr):
+	word = ' '.join(word_arr[1:(len(word_arr))])
+	if word in phonetic_words:
+		index = phonetic_words.index(word)
+		print '{} (word exists already: {})'.format(word, words[index])
+	else:
+		print word
 
-word_to_output = ''
-current_phoneme = 'START_WORD'
+word = ['START_WORD']
 while True:
-    next_phoneme = get_next_phoneme(current_phoneme, random.random())
-    if next_phoneme == 'END_WORD':
-        if not_in_dict(word_to_output):
-            print word_to_output
-        word_to_output = ''
-        current_phoneme = 'START_WORD'
-        time.sleep(.5)
-    else:
-        word_to_output = next_phoneme if len(word_to_output) == 0 else word_to_output + ' ' + next_phoneme
-        current_phoneme = next_phoneme
+	next_phoneme = get_next_phoneme(word[len(word)-1], random.random())
+	if next_phoneme == 'END_WORD':
+		present(word)
+		word = ['START_WORD']
+		time.sleep(.5)
+	else:
+		word.append(next_phoneme)
+		phoneme = next_phoneme
--- bin/list_most_likely_words.py
+++ bin/list_most_likely_words.py
@@ -3,20 +3,20 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 from lib import file
 
-phoneme_chain_prob = file.load('ranked_most_likely_next_phonemes_per_phoneme')
+ranked_most_likely_next_phonemes_per_phoneme = file.load('ranked_most_likely_next_phonemes_per_phoneme')
 
 def present(word):
 	return ' '.join(word[1:(len(word)-1)])
 
-def next_phoneme(current_word, level):
-	current_phoneme = current_word[len(current_word) - 1]
+def next_phoneme(word):
+	current_phoneme = word[len(word) - 1]
 
-	if (current_phoneme == 'END_WORD' or len(current_word) > 20):
-		print present(current_word)
+	if (current_phoneme == 'END_WORD' or len(word) > 20):
+		print present(word)
 	else:
-		for phoneme in phoneme_chain_prob[current_phoneme]:
-			grown_word = current_word[:]
+		for phoneme in ranked_most_likely_next_phonemes_per_phoneme[current_phoneme]:
+			grown_word = word[:]
 			grown_word.append(phoneme)
-			next_phoneme(grown_word, level + 1)
+			next_phoneme(grown_word)
 
-next_phoneme(['START_WORD'], 1)
+next_phoneme(['START_WORD'])
