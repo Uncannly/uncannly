f62dcea79a4cf059730023a99ef6f4e62ac83049
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-11-19 17:12:29 -0800

    simplify calculation of next random phoneme by ensuring ordering of cumulative distribution

--- bin/generate_random_likely_words.py
+++ bin/generate_random_likely_words.py
@@ -5,33 +5,11 @@ from lib import file, format, exists
 
 cumulative_distributions = file.load('cumulative_distributions')
 
-# THIS VERSION ONLY WORKS IF THE PROBABILITIES ARE ORDERED, 
-# WHICH THEY MOSTLY ARE BUT NOT PERFECTLY...
-# def next_phoneme(current_phoneme, random_number):
-# 	return next(phoneme
-# 		for phoneme, probability 
-# 		in cumulative_distributions[current_phoneme].iteritems() 
-# 		if probability >= random_number
-# 	)
-
-def next_phoneme(phoneme, random_number):
-	current_next_phoneme_chance = 2.0
-	current_next_phoneme = ''
-	for transition_phoneme, chance in cumulative_distributions[phoneme].iteritems():
-		if chance >= random_number and chance < current_next_phoneme_chance:
-			current_next_phoneme_chance = chance
-			current_next_phoneme = transition_phoneme
-	return current_next_phoneme
-
-def test_for_orderedness(phoneme):
-	previous_phoneme = ''
-	previous_chance = 0
-	for next_phoneme, next_chance in cumulative_distributions[phoneme].iteritems():
-		print next_phoneme, next_chance
-		if next_chance < previous_chance:
-			print 'what'
-		previous_phoneme = next_phoneme
-		previous_chance = next_chance
+def next_phoneme(current_phoneme, random_number):
+	return next(step['next_phoneme']
+		for step in cumulative_distributions[current_phoneme]
+		if step['accumulated_probability'] >= random_number
+	)
 
 def present(word):
 	word = format.format(word)
@@ -52,5 +30,3 @@ while True:
 		time.sleep(0.2)
 	else:
 		word.append(phoneme)
-
-test_for_orderedness('IY')
--- lib/for_random.py
+++ lib/for_random.py
@@ -1,11 +1,14 @@
 def cumulative_distribution(next_phoneme_occurrences):
 	total_occurrences_of_next_phonemes = sum(next_phoneme_occurrences.itervalues())
-	cumulative_distribution = {}
+	cumulative_distribution = []
 	accumulated_probability = 0.0
 
 	for next_phoneme, occurrences in next_phoneme_occurrences.iteritems():
 		probability = occurrences / total_occurrences_of_next_phonemes
 		accumulated_probability += probability
-		cumulative_distribution[next_phoneme] = accumulated_probability
+		cumulative_distribution.append({
+			'next_phoneme': next_phoneme, 
+			'accumulated_probability': accumulated_probability
+		})
 		
 	return cumulative_distribution
