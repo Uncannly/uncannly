26638cf0f063b365127af9276af830b99abfc0c7
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-11-20 17:18:08 -0800

    second endpoint for multiple most probable words [#134524963]

--- app/app.py
+++ app/app.py
@@ -10,22 +10,27 @@ from lib.api import random_probable_word, most_probable_words
 app = Flask(__name__)
 CORS(app)
 
-# @app.route('/words')
-# def words():
-#     style = request.args.get('style') # sorted or random
-#     filter = request.args.get('filter') # by averaging, by continued product, or none
-#     weighted_by_frequency = request.args.get('weighted_by_frequency') # true or false
-#     include_real_words = request.args.get('include_real_words') # true or false
-#     return_count = request.args.get('return_count') # how many words to return
-#     pool_count = request.args.get('pool_count') # how many words to generate to draw from
+@app.route('/words')
+def words():
+    style = request.args.get('style') # sorted or random
+    filter = request.args.get('filter') # by averaging, by continued product, or none
+    weighted_by_frequency = request.args.get('weighted_by_frequency') # true or false
+    # include_real_words = request.args.get('include_real_words') # true or false
+    return_count = request.args.get('return_count') # how many words to return
+    # pool_count = request.args.get('pool_count') # how many words to generate to draw from
 
-#     words = 
-#     return json.dumps(words)
+    words = most_probable_words.get(
+    	style=style, 
+    	filter=filter, 
+    	weighted_by_frequency=weighted_by_frequency, 
+    	return_count=return_count
+    ) 
+    return json.dumps(words)
 
 @app.route('/random_word')
 def random_word():
     word = random_probable_word.get()
-    return json.dumps(word, ensure_ascii = False)
+    return json.dumps(word, ensure_ascii=False)
 
 if __name__ == "__main__":
     http_server = WSGIServer(('0.0.0.0', int(os.getenv("PORT", 5000))), app)
--- lib/api/most_probable_words.py
+++ lib/api/most_probable_words.py
@@ -1,2 +1,22 @@
+import random, os, sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+from lib import file
+
 def get(style, filter, weighted_by_frequency, return_count):
-	pass
+	frequency_weighting = 'weighted' if weighted_by_frequency else 'unweighted'
+	most_probable_words = file.load(
+		'most_probable_words_by_{}_{}'.format(filter, frequency_weighting)
+	)
+	if style == 'sorted':
+		return sorted(
+			most_probable_words,
+			key = most_probable_words.get,
+			reverse = True
+		)[0:int(return_count)]
+	else:
+		output = []
+		words = most_probable_words.keys()
+		for i in range(0, int(return_count)):
+			output.append(random.choice(words))
+		return output
