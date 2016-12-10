da1e6b3a92b5c56f2416834f820e3046423bb828
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-12-09 18:36:06 -0800

    copy to clipboard buttons [#134788065]

--- app/static/controls.js
+++ app/static/controls.js
@@ -1,33 +1,36 @@
-$(".random-word button").click(function(e) {
-	let url = '/random-word'
-	const data = []
-	if ($(".random-word .unweighted").is(':checked')) data.push('unweighted')
-	if ($(".random-word .exclude-real").is(':checked')) data.push('exclude-real')
-	if (data.length > 0) url += '?' + data.join('&')
+$(".random-word button.refresh").click(function(e) {
+    let url = '/random-word'
+    const data = []
+    if ($(".random-word .unweighted").is(':checked')) data.push('unweighted')
+    if ($(".random-word .exclude-real").is(':checked')) data.push('exclude-real')
+    if (data.length > 0) url += '?' + data.join('&')
 
     $.ajax({
         url: url,
         success: function(result) {
-        	$(".random-word span").html(result);
+            $("#random-word").html(result);
         }
     });
 });
 
-$(".words button").click(function(e) {
-	let url = '/words'
-	const data = []
-	const returnCount = $(".words .return-count").val()
-	if (returnCount) data.push(`return-count=${returnCount}`)
-	if ($(".words .averaging").is(':checked')) data.push("averaging")
-	if ($(".words .random-selection").is(':checked')) data.push("random-selection")
-	if ($(".words .unweighted").is(':checked')) data.push('unweighted')
-	if ($(".words .exclude-real").is(':checked')) data.push('exclude-real')
-	if (data.length > 0) url += '?' + data.join('&')
+$(".words button.refresh").click(function(e) {
+    let url = '/words'
+    const data = []
+    const returnCount = $(".words .return-count").val()
+    if (returnCount) data.push(`return-count=${returnCount}`)
+    if ($(".words .averaging").is(':checked')) data.push("averaging")
+    if ($(".words .random-selection").is(':checked')) data.push("random-selection")
+    if ($(".words .unweighted").is(':checked')) data.push('unweighted')
+    if ($(".words .exclude-real").is(':checked')) data.push('exclude-real')
+    if (data.length > 0) url += '?' + data.join('&')
 
     $.ajax({
-    	url: url,
-    	success: function(result) {
-    		$(".words span").html(JSON.parse(result).join('<br>'));
-    		}
-	});
+        url: url,
+        success: function(result) {
+            $("#words").html(JSON.parse(result).join('<br>'));
+        }
+    });
 });
+
+new Clipboard('#copy-random-word');
+new Clipboard('#copy-words');
