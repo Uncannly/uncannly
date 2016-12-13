58d61805059587f88f8abb6231f4278ea1612ec6
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-12-12 23:47:22 -0800

    remove unnecessary file [#135963913]

--- app/api.py
+++
@@ -1,7 +0,0 @@
-import random, os, sys
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
-
-from lib.mode import get_by_mode
-
-def api(mode, args):
-	return get_by_mode(mode=mode, interface='api', args=args)
--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 from flask import Flask, request, render_template
 from gevent.wsgi import WSGIServer
 
-from api import api
+from lib.mode import get_by_mode
 
 app = Flask(__name__)
 CORS(app)
@@ -15,7 +15,7 @@ def root():
     return render_template('index.html')
 
 def route(mode, request):
-    request_args = {
+    args = {
         'return_count': request.args.get('return-count'),
         'random_selection': request.args.get('random-selection'), 
         'scoring_method': request.args.get('scoring-method'), 
@@ -27,7 +27,7 @@ def route(mode, request):
         'unweighted': request.args.get('unweighted'), 
         'exclude_real': request.args.get('exclude-real')
     }
-    response = api(mode, request_args)
+    response = get_by_mode(mode=mode, interface='api', args=args)
     return json.dumps(response, ensure_ascii=False)
 
 @app.route('/random-word')
