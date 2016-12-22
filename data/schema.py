87b495adf61e5909b910a0195121725746ce0734
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-12-21 22:20:25 -0800

    minor improve

--- data/schema.py
+++ data/schema.py
@@ -22,20 +22,22 @@ class Schema:
 
 	def words(self, words):
 		sql_array = []
-		for word in words:
-			sql_array.append("('{}', '{}')".format(word[0].replace("'", "''"), word[1]))
+		for word, pronunciation in words:
+			sql_array.append("('{}', '{}')".format(
+				word.replace("'", "''"), pronunciation
+			))
 		sql_string = "insert into words (word, pronunciation) values"
 		sql_string += ", ".join(sql_array)
 		self.database.execute(sql_string)
 
-	def phonemes(self, phonemes, phonemes_unweighted, unstressed):
+	def phonemes(self, phonemes, next_phonemes_unweighted, unstressed):
 		sql_array = []
-		for phoneme, sorted_list_of_next_phoneme_and_probability_tuples in phonemes.iteritems():
+		for phoneme, next_phonemes_for_this_phoneme in phonemes.iteritems():
 			sql_array.append("('{}', '{}', '{}', '{}')".format(
 				phoneme, 
 				unstressed, 
-				json.dumps(sorted_list_of_next_phoneme_and_probability_tuples),
-				json.dumps(phonemes_unweighted[phoneme]),
+				json.dumps(next_phonemes_for_this_phoneme),
+				json.dumps(next_phonemes_unweighted[phoneme]),
 			))
 		sql_string = "insert into phonemes (phoneme, unstressed, \
 			next_phonemes, next_phonemes_unweighted) values"
