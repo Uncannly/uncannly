e7822858fe0f9a74eb2e252c72abcb3eecc991fa
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-12-22 18:31:20 -0800

    simplify args in lib/mode [#136545775]

--- lib/mode.py
+++ lib/mode.py
@@ -3,61 +3,39 @@ from modes.random_mode import RandomMode
 from case_conversion import kebab_to_snake
 
 def get_by_mode(mode, interface, args):
-	get_args = {}
+	get_args = {'selection': None, 'scoring_method': 'integral_product'}
 
-	if args['pool']:
-		get_args['pool'] = int(args['pool'])
-	else:
-		get_args['pool'] = 45
+	get_args['pool'] = int(args['pool']) if args['pool'] else 45
 
 	if args['selection'] is not None:
-		if args['selection'] == '':
-			get_args['selection'] = args['pool']
-		else:
-			get_args['selection'] = int(args['selection'])
+		get_args['selection'] = args['pool'] if args['selection'] == '' \
+			else int(args['selection'])
 
 	if args['top_selection'] and mode == 'random':
 		get_args['selection'] = int(args['top_selection'])
 	if args['random_selection'] and mode == 'top':
 		get_args['selection'] = int(args['random_selection'])
 
-	if not get_args.get('selection'):
-		get_args['selection'] = None
-
-	if args.get('score_by_mean_arithmetic'):
-		get_args['scoring_method'] = 'mean_arithmetic'
-	if args.get('score_by_mean_geometric'):
-		get_args['scoring_method'] = 'mean_geometric'
-	if args.get('score_by_integral_sum'):
-		get_args['scoring_method'] = 'integral_sum'
-	if args.get('score_by_integral_product'):
-		get_args['scoring_method'] = 'integral_product'
-
-	if args.get('scoring_method') == None:
-		get_args['scoring_method'] = 'integral_product'
-	else:
+	if args['scoring_method']:
 		get_args['scoring_method'] = kebab_to_snake(args['scoring_method'])
-
-	if args['score_threshold']:
-		get_args['score_threshold'] = float(args['score_threshold'])
 	else:
-		get_args['score_threshold'] = None
+		scoring_methods = [
+			'mean_arithmetic', 'mean_geometric', 'integral_sum', 'integral_product'
+		]
+		for method in scoring_methods:
+			if args.get('score_by_{}'.format(method)):
+				get_args['scoring_method'] = method
 
-	if args['unweighted'] == None or args['unweighted'] == False:
-		get_args['unweighted'] = False
-	else:
-		get_args['unweighted'] = True
+	get_args['score_threshold'] = float(args['score_threshold']) \
+		if args['score_threshold'] else None
 
-	if args['unstressed'] == None or args['unstressed'] == False:
-		get_args['unstressed'] = False
-	else:
-		get_args['unstressed'] = True
+	get_args['unweighted'] = args['unweighted'] or args['unweighted'] == ''
+	get_args['unstressed'] = args['unstressed'] or args['unstressed'] == ''
+	get_args['exclude_real'] = args['exclude_real'] or args['exclude_real'] == ''
 
-	if args['exclude_real'] == None or args['exclude_real'] == False:
-		get_args['exclude_real'] = False
-	else:
-		get_args['exclude_real'] = True
-
+	print 'args'
+	print args
+	print 'get_args'
 	print get_args
 
 	getter = TopMode if mode == 'top' else RandomMode
